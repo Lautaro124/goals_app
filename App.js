06c5233202f626437b57d983e396f9c3a24694c8
lautaro.gonzalez <lautaro.gonzalez@ctl.com.ar>
2023-03-28 15:34:55 -0300

    Input styling

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
-export default function App() {
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inpoutContainer}>
+        <TextInput style={styles.textInputObject} placeholder='Write me' />
+        <Button title='Touch me' />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals..</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1
   },
-});
+  inpoutContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#cccccc',
+    marginBottom: 26
+  },
+  textInputObject: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+    borderRadius: 4
+  },
+  goalsContainer: {
+    flex: 6
+  }
+})
